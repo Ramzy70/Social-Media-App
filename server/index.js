b5cb1ba6ffa48647d02ffb810d965f32bd32b188
Amira Ramzi <ramzi70174@gmail.com>
2023-10-23 21:40:48 +0100

    front and back both done without linking

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const mongoose = require("mongoose")
+require('dotenv').config();
+const helmet = require("helmet")
+const morgan = require("morgan")
+const colors = require('colors')
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
+
+const app = express()
+
+const connectDB = async () => {
+    try {
+        const conn =await mongoose.connect(process.env.MONGO_URI)
+
+        console.log(`MongoDB Connected : ${conn.connection.host}`.cyan.underline)
+    } catch(error) {
+        console.log(error)
+        process.exit()
+    }
+}
+
+connectDB()
+
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/posts", postRoute)
+
+
+app.listen(5000, () => {
+    console.log("Backend server is running")
+})
